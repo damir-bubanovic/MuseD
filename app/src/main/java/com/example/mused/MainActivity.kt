fe07f645321b0721bef6bcc5684d62d55b4b74f0
Damir Bubanovic <damir.bubanovic@yahoo.com>
2026-06-03 13:17:54 +0200

    Add polished splash screen

--- app/src/main/java/com/example/mused/MainActivity.kt
+++ app/src/main/java/com/example/mused/MainActivity.kt
@@ -10,9 +10,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.example.mused.ui.screens.HomeScreen
 import com.example.mused.ui.theme.MuseDTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
